b0c250a97f5d5ab60b4c9aa17252cd549e79c57b
Salazarda <46509817+Salazarda@users.noreply.github.com>
2019-06-12 16:22:54 -0400

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,138 @@
+// @TODO: YOUR CODE HERE!
+// @TODO: YOUR CODE HERE!
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+  
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+  
+    // Append SVG element
+    var svg = d3
+      .select("#scatter")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+  
+    // Append group element
+    var chartGroup = svg.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+    // Read CSV
+    d3.csv("../assets/data/data.csv")
+      .then(function(HwData) {
+        // parse data
+        HwData.forEach(function(data) {
+          data.poverty = +data.poverty;
+          data.smokes = +data.smokes;
+        });
+  
+        // create scales
+        var xLinearScale = d3.scaleLinear()
+          .domain(d3.extent(HwData, d => d.poverty))
+          .range([0, width]);
+  
+        var yLinearScale = d3.scaleLinear()
+          .domain([0, d3.max(HwData, d => d.smokes)])
+          .range([height, 0]);
+  
+        // create axes
+        var xAxis = d3.axisBottom(xLinearScale).ticks();
+        var yAxis = d3.axisLeft(yLinearScale).ticks();
+  
+        // append x axis
+        chartGroup.append("g")
+          .attr("transform", `translate(0, ${height})`)
+          .call(xAxis);
+  
+        // label x axis
+        chartGroup.append("text")
+            .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom-5) + ")")
+            .style("text-anchor", "middle")
+            .text("Poverty (%)")
+            .attr("font-size", "15");  
+
+        // append y axis
+        chartGroup.append("g")
+          .call(yAxis);
+        
+        // label y axis
+        chartGroup.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x",0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("Smoking (%)")
+            .attr("font-size", "15");
+
+        // append circles
+        var circlesGroup = chartGroup.selectAll("circle")
+          .data(HwData)
+          .enter()
+          .append("circle")
+          .attr("cx", d => xLinearScale(d.poverty))
+          .attr("cy", d => yLinearScale(d.smokes))
+          .attr("r", "12")
+          .classed("stateCircle", true);
+        
+        // append state abbreviations
+        var abbrGroup = chartGroup.selectAll("text")
+        .data(HwData)
+        .enter()
+        .append("text")
+        .attr("x", d => xLinearScale(d.poverty))
+        .attr("y", d => yLinearScale(d.smokes)+4)
+        .attr("font-size", "12")
+        .text(d => d.abbr)
+        .classed("stateText", true);
+
+        // Step 1: Initialize Tooltip
+        var toolTip = d3.tip()
+          .attr("class", "tooltip")
+          .offset([80, -60])
+          .html(function(d) {
+            return (`<strong>${d.state}<br>Poverty: ${d.poverty}%<br>Smoking: ${d.smokes}
+            %`);
+          });
+  
+        // Step 2: Create the tooltip in chartGroup.
+        chartGroup.call(toolTip);
+  
+        // Step 3: Create "mouseover" event listener to display tooltip
+        circlesGroup.on("mouseover", function(d) {
+          toolTip.show(d, this);
+        })
+        // Step 4: Create "mouseout" event listener to hide tooltip
+          .on("mouseout", function(d) {
+            toolTip.hide(d);
+          });
+      });
+  }
+  
+  // When the browser loads, makeResponsive() is called.
+  makeResponsive();
+  
+  // When the browser window is resized, makeResponsive() is called.
+  d3.select(window).on("resize", makeResponsive);
